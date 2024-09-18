0f810f6dccac31eb6f01b276524201d6ba73d7bb
NINA <ninajoy2006@gmail.com>
2024-09-18 17:10:25 +0800

    Create Activity 1.cpp

---
+++ nina-2006-c/Activity 1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+
+string name = "Nina";
+char middle = 'D';
+string last = "Parica";
+int age = 18;
+
+cout <<"Name:" << name << endl; 
+cout <<"MiddleName:" << middle << endl;
+cout <<"LastName:" << last << endl;
+cout <<"Age:" << age << endl;
+
+return 0;
+ }        
+ 
+
+ 
